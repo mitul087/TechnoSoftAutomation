c079fd7309ea573ba543d21fc9eac89eb9ef8f23
shumonnyc <bethere11102@gmail.com>
2016-08-16 10:07:25 -0400

    EditedCLickMethods

--- src/main/java/com/priceline/baseActions/ClickMethods.java
+++ src/main/java/com/priceline/baseActions/ClickMethods.java
@@ -1,7 +1,45 @@
 package com.priceline.baseActions;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import com.priceline.utilities.GlobalConfig;
 
 public class ClickMethods extends GlobalConfig{
+	
+	public FindMethods  fm = null;
+	public WebElement element = null;
+	
+	
+	/*
+	 * This constructor creates ClickMethods Object
+	 * 
+	 * 
+	 */
+	
+	public ClickMethods(){
+		
+		
+		
+		
+	}
+	
+	     
+	/*
+	 * This method locates elements
+	 * ataul
+	 * 8-11-2016 16:41
+	 */
+	
+	public void clickOnElement(String locator, String attributeOfLocator, String isShowStopper){
+		
+		getElement(locator,attributeOfLocator,isShowStopper);
+		
+		
+		
+		
+		
+		
+	}
 
 }
