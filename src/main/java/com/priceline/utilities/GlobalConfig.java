e1cc206f6351f735e40588e014342df75e5ee4c2
msayem2 <msayem112@gmail.com>
2016-08-16 12:12:09 -0400

    updated GlobalConfig.java

--- src/main/java/com/priceline/utilities/GlobalConfig.java
+++ src/main/java/com/priceline/utilities/GlobalConfig.java
@@ -1,41 +1,37 @@
 package com.priceline.utilities;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 public abstract class GlobalConfig {
-	
+
 	public static WebDriver myDriver = null;
 	private static String siteLink = "https://www.priceline.com/";
-	
+
 	/* File Paths */
 	private static String operatingSystem = System.getProperty("os.name");
 	private static String fileSeparator = System.getProperty("file.separator");
 	private static String currentDir = System.getProperty("user.dir");
-	
-	private static String chromeDriverPath = currentDir + fileSeparator + "macChromeDriver" + fileSeparator + "chromedriver";
-	/* end of file paths*/
-	
-	public static void ConfigDrivers(){
+
+	private static String chromeDriverPath = currentDir 
+			+ fileSeparator
+			+ "macChromeDriver" 
+			+ fileSeparator 
+			+ "chromedriver";
+
+	/* end of file paths */
+
+	public static void ConfigDrivers() {
 		System.out.println("INITIALIZING DRIVER");
 		System.out.println(operatingSystem);
 		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
 		myDriver = new ChromeDriver();
-		//myDriver = new FirefoxDriver();
-		myDriver.navigate().to( siteLink );
+		// myDriver = new FirefoxDriver();
+		myDriver.navigate().to(siteLink);
 	}
-	
-	
-	public static WebElement getElement(String locator, String attributeOfLocator){
-		WebElement result = null;
-		
-		return result;
-	}
-	
-	public static void terminateDrivers(){
-		
+
+	public static void terminateDrivers() {
+
 		myDriver.close();
 		myDriver.quit();
 	}
